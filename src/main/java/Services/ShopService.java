d3f6924f6f9f8ec1abe3a2b0310588d9358fc1ba
Osama Sado <osama.sadow@gmail.com>
2026-07-24 08:18:31 +0200

    change order date type from LocalDate to Instant

--- src/main/java/Services/ShopService.java
+++ src/main/java/Services/ShopService.java
@@ -3,14 +3,12 @@ package Services;
 import DTO.Order;
 import DTO.Product;
 import Inertfaces.OrderRepoInterface;
-import Repositories.OrderListRepo;
 import Repositories.ProductRepo;
 import Utils.ConsoleColors;
 import Utils.OrderStatus;
 
-import java.time.LocalDate;
+import java.time.Instant;
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class ShopService {
     private final ProductRepo productRepo;
@@ -34,7 +32,7 @@ public class ShopService {
             totalAmount += product.amount();
             totalPrice += product.amount() * product.price();
         }
-        Order order = new Order(UUID.randomUUID().toString(), LocalDate.now(), customerId, productsToOrder, totalPrice, totalAmount, OrderStatus.IN_PROCESSING);
+        Order order = new Order(UUID.randomUUID().toString(), Instant.now(), customerId, productsToOrder, totalPrice, totalAmount, OrderStatus.IN_PROCESSING);
         orderRepo.addOrder(order);
         System.out.println(ConsoleColors.GREEN + "Your order is created." + ConsoleColors.RESET);
     }
--- src/test/java/Repositories/OrderListRepoTest.java
+++ src/test/java/Repositories/OrderListRepoTest.java
@@ -5,7 +5,7 @@ import DTO.Product;
 import Utils.OrderStatus;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
+import java.time.Instant;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,12 +21,12 @@ class OrderListRepoTest {
         Product product1 = new Product("1", "Laptop", "HP Laptop 2026",10, 5);
         Product product2 = new Product("2", "Maus", "Wireless Maus",5, 10);
 
-        Order order1 = new Order("1", LocalDate.now(), 1, List.of(product1, product2), 100,15, OrderStatus.IN_PROCESSING);
+        Order order1 = new Order("1", Instant.now(), 1, List.of(product1, product2), 100,15, OrderStatus.IN_PROCESSING);
 
         Product product3 = new Product("1", "Laptop", "HP Laptop 2026",100, 1);
         Product product4 = new Product("2", "Maus", "Wireless Maus",10, 1);
 
-        Order order2 = new Order("2", LocalDate.now(), 2, List.of(product3, product4), 110,2, OrderStatus.IN_PROCESSING);
+        Order order2 = new Order("2", Instant.now(), 2, List.of(product3, product4), 110,2, OrderStatus.IN_PROCESSING);
 
         orderListRepo.addOrder(order1);
         orderListRepo.addOrder(order2);
@@ -43,7 +43,7 @@ class OrderListRepoTest {
         Product product1 = new Product("1", "Laptop", "HP Laptop 2026",10, 5);
         Product product2 = new Product("2", "Maus", "Wireless Maus",5, 10);
 
-        Order order = new Order("1", LocalDate.now(), 1, List.of(product1, product2), 100,15, OrderStatus.IN_PROCESSING);
+        Order order = new Order("1", Instant.now(), 1, List.of(product1, product2), 100,15, OrderStatus.IN_PROCESSING);
 
         orderListRepo.addOrder(order);
 
@@ -59,7 +59,7 @@ class OrderListRepoTest {
         Product product1 = new Product("1", "Laptop", "HP Laptop 2026",10, 5);
         Product product2 = new Product("2", "Maus", "Wireless Maus",5, 10);
 
-        Order order = new Order("1", LocalDate.now(), 1, List.of(product1, product2), 100,15, OrderStatus.IN_PROCESSING);
+        Order order = new Order("1", Instant.now(), 1, List.of(product1, product2), 100,15, OrderStatus.IN_PROCESSING);
 
         orderListRepo.addOrder(order);
         orderListRepo.deleteOrder(order.id());
@@ -76,7 +76,7 @@ class OrderListRepoTest {
         Product product1 = new Product("1", "Laptop", "HP Laptop 2026",10, 5);
         Product product2 = new Product("2", "Maus", "Wireless Maus",5, 10);
 
-        Order order = new Order("1", LocalDate.now(), 1, List.of(product1, product2), 100,15, OrderStatus.IN_PROCESSING);
+        Order order = new Order("1", Instant.now(), 1, List.of(product1, product2), 100,15, OrderStatus.IN_PROCESSING);
 
         orderListRepo.addOrder(order);
         Order expected = orderListRepo.getOrderById(order.id());
